548c4e430e232031a6f1a370c5cdda996dc04945
Ulises Basualdo <ulimiramar@gmail.com>
2024-10-24 21:27:06 -0300

    agrega estructura inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ParallaxHeroComponent } from "./atomic-ui/organisms/parallax-hero/parallax-hero.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ParallaxHeroComponent],
   template: `
+  <app-parallax-hero />
     <h1>Welcome to {{ title }} my friend!</h1>
     <router-outlet />
   `,
---
+++ src/app/shared/directives/scroll-animations.directive.ts
@@ -0,0 +1,27 @@
+import { AfterViewInit, Directive, ElementRef, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appScrollAnimations]',
+  standalone: true,
+})
+export class ScrollAnimationsDirective implements AfterViewInit {
+  @Input('animationInput') animationInput: string = "";
+  @Input('animationOutput') animationOutput: string = "fadeOut";
+  @Input('opacityEnd') opacityEnd: string = "1";
+  @Input('animationDelay') delay: string = "0s";
+  @Input('isOneTime') isOneTime: boolean = false;
+
+  private observer!: IntersectionObserver;
+
+  constructor(private el: ElementRef<HTMLElement>) { }
+
+  ngAfterViewInit() {
+    this.observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        this.el.nativeElement.style.opacity = entry.isIntersecting ? this.opacityEnd : '0';
+        this.el.nativeElement.classList.toggle(this.animationInput, entry.isIntersecting);
+      });
+    });
+    this.observer.observe(this.el.nativeElement);
+  }
+}
