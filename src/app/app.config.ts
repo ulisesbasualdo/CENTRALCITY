ef38685990d683c58ec98bf4603934e3da87cb5a
Ulises Basualdo <ulimiramar@gmail.com>
2025-05-01 15:40:20 -0300

    guardado general y se elimina husky y demas linteres

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,8 @@
-import { ApplicationConfig, provideExperimentalZonelessChangeDetection, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideExperimentalZonelessChangeDetection,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
